ac8a0bf28c7079e2e6c26d477a66ba0d6135dbbf
Kourtney Smith <klsmith5370@gmail.com>
2024-01-12 22:15:40 -0500

    Functionality for deleting tickets

--- app/api/Tickets/route.js
+++ app/api/Tickets/route.js
@@ -2,7 +2,6 @@ import Ticket from "@/app/(models)/Ticket"
 import { NextResponse } from "next/server"
 
 export async function POST(req) {
-    console.log("POST RAN")
     try {
         const body = await req.json()
         const ticketData = body.formData
