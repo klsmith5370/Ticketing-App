77bd1f74b9957f751f22cdfb51b8fbf6848441e9
Kourtney Smith <klsmith5370@gmail.com>
2024-01-12 21:43:30 -0500

    Brought in all ticket data

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -3,30 +3,30 @@ import PriorityDisplay from "./PriorityDisplay"
 import ProgressDisplay from "./ProgessBar"
 import StatusDisplay from "./StatusDisplay"
 
-const TicketCard = () => {
+const TicketCard = ({ ticket }) => {
     return (
        <div className="flex flex-col bg-black hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
             
             <div className="flex mb-3">
-                <PriorityDisplay />
+                <PriorityDisplay priority={ticket.priority} />
                 <div className="ml-auto">
                     <DeleteBlock />
                 </div>  
             </div>
 
-            <h4>Ticket Title</h4>
+            <h4>{ticket.title}</h4>
             <hr className="h-px border-0 bg-page mb-2" />
-            <p className="whitespace-pre-wrap">this is the ticket description</p>
+            <p className="whitespace-pre-wrap">{ticket.description}</p>
 
             <div className="flex-grow"></div>
 
             <div className="flex mt-2">
                 <div className="flex flex-col">
-                    <p className="text-xs my-1">01/12/2024 10:43pm</p>
-                    <ProgressDisplay />
+                    <p className="text-xs my-1">{ticket.createdAt}</p>
+                    <ProgressDisplay progress={ticket.progress} />
                 </div>
                 <div className="ml-auto flex items-end">
-                    <StatusDisplay />
+                    <StatusDisplay status={ticket.status} />
                 </div>
             </div>
 
